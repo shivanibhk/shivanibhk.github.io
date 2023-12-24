e89f1853c36a119183ec4833a38142718df7c7c2
Shivani Hukkeri <hshivani08@gmail.com>
2023-12-24 04:47:08 -0800

    added scrolling framer motion graphic, ability to scale 3D model according to small or large screen size (but it's not working as desired on my laptop), percentage the 3D model has loaded instead of buffering whole website

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -24,6 +24,28 @@ const Hero = () => {
 
       {/* 3D computer model, underneath text */}
       <ComputersCanvas />
+      {/* xs = extra small devices
+          on extra small devices it'll have bottom 10
+          else bottom 32
+      */}
+      <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'> 
+        <a href='#about'>
+          <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
+            {/* create framer motion */}
+            <motion.dev
+              animate={{
+                y: [0, 24, 0] // moves 24 px up and down
+              }}
+              transition={{
+                duration: 1.5,
+                repeat: Infinity,
+                repeatType: 'loop'
+              }}
+              className='w-3 h-3 rounded-full bg-secondary mb-1'
+            />
+          </div>
+        </a>
+      </div>
     </section>
   )
 }
--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,18 +1,17 @@
-import { Suspense, useEffect, useState } from 'react';
-import { Canvas } from '@react-three/fiber';
-import { OrbitControls, Preload, useGLTF } from '@react-three/drei';
+import React, { Suspense, useEffect, useState } from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 
-import CanvasLoader from '../Loader';
+import CanvasLoader from "../Loader";
 
-const Computers = () => {
-  const computer = useGLTF('./desktop_pc/scene.gltf')
+const Computers = ({ isMobile }) => {
+  const computer = useGLTF("./desktop_pc/scene.gltf");
 
   return (
     <mesh>
       {/* need to create a light to see it; see react-three-fiber doc
       to see diff ways to add lights */}
-      <hemisphereLight intensity={0.15}
-      groundColor="black" />
+      <hemisphereLight intensity={0.15} groundColor="black" />
       {/* point light that shows up as glare on computer */}
       <pointLight intensity={1} />
       {/* will see computer after loading primitive into Canvas */}
@@ -27,21 +26,45 @@ const Computers = () => {
       />
       <primitive
         object={computer.scene}
-        scale={0.75}
-        position={[0, -3.25, -1.5]}
+        scale={isMobile ? 0.7 : 0.75}
+        position={isMobile ? [0, -3,- 2.2] : [0, -3.25, -1.5]}
         rotation={[-0.01, -0.2, -0.1]}
       />
     </mesh>
-  )
-}
+  );
+};
 
 const ComputersCanvas = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    // checks to see if we're on mobile
+    const mediaQuery = window.matchMedia("(max-width: 500px)");
+
+    // sets isMobile variable to true or false
+    setIsMobile(mediaQuery.matches);
+
+    // handles changes to media query
+    const handleMediaQueryChange = (event) => {
+      setIsMobile(event.matches);
+    };
+
+    // add callback fnc as listener for changes to media query
+    mediaQuery.addEventListener("change", handleMediaQueryChange);
+
+    // remove listener when component is unmounted
+    return () => {
+      mediaQuery.removeEventListener("change", handleMediaQueryChange);
+    };
+  }, []);
+
   return (
     <Canvas
       frameloop='demand'
       shadows
       camera={{ position: [20, 3, 5], fov: 25 }} //where are we looking at this model from
       // pos:[x,y,z], fov: field of view (how wide our fov is going to be)
+      dpr={[1, 2]}
       gl={{ preserveDrawingBuffer: true }} // needs to be here to properly render 3D model
     >
       <Suspense fallback={<CanvasLoader />}>
@@ -51,7 +74,7 @@ const ComputersCanvas = () => {
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2} // makes it so that we can only rotate it along a specific access
         />
-        <Computers />
+        <Computers isMobile={isMobile} />
       </Suspense>
       <Preload all />
     </Canvas>
