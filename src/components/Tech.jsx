e54074fc417f9586e7d2aaed23e496c5651ef0a4
Shivani Hukkeri <hshivani08@gmail.com>
2023-12-24 16:05:24 -0800

    implemented technologies section; created balls for each item and animated the balls using fiber and drei

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import { BallCanvas } from "./canvas"; // the actual balls
+import { SectionWrapper } from "../hoc";
+import { technologies } from "../constants";
+
 
 const Tech = () => {
   return (
-    <div>Tech</div>
+    // flex-wrap: full length of line if screen is too tight
+    <div className='flex flex-row flex-wrap justify-center gap-10'>
+      {technologies.map((technology) => (
+        // for each technology we want to return a div
+        <div className="w-28 h-28" key={technology.name}>
+          <BallCanvas icon={technology.icon} />
+        </div>
+      ))}
+    </div>
   )
 }
 
-export default Tech
+export default SectionWrapper(Tech, "");
--- src/components/canvas/Ball.jsx
+++ src/components/canvas/Ball.jsx
@@ -1,9 +1,53 @@
-import React from 'react'
+import React, { Suspense } from 'react'
+import { Canvas } from '@react-three/fiber'
+import {
+  Decal, Float, OrbitControls, Preload, useTexture
+} from '@react-three/drei' // decal: texture
 
-const Ball = () => {
+import CanvasLoader from '../Loader'
+
+const Ball = (props) => {
+  // the imgUrl are the icons that are being passed into BallCanvas
+  const [decal] = useTexture([props.imgUrl]);
+
   return (
-    <div>Ball</div>
+    <Float speed={1.75} rotationIntensity={1} floatIntensity={2}>
+      <ambientLight intensity={0.5} />
+      <directionalLight position={[0, 0, 0.05]} />
+      <mesh castShadow receiveShadow scale={2.75}>
+        {/* render a icosahedron */}
+        <icosahedronGeometry args={[1, 1]} />
+        <meshStandardMaterial
+          color='#fff8eb'
+          polygonOffset
+          polygonOffsetFactor={-5}
+          flatShading
+        />
+        <Decal
+          position={[0,0,1]}
+          rotation={[2*Math.PI, 0, 6.25]}
+          flatShading
+          map={decal}
+        />
+      </mesh>
+    </Float>
   )
 }
 
-export default Ball
+const BallCanvas = ({icon}) => (
+  <Canvas
+    frameloop='demand'
+    gl={{ preserveDrawingBuffer: true }} // needs to be here to properly render 3D model
+  >
+    <Suspense fallback={<CanvasLoader />}>
+      {/* allows us to move model left and right */}
+      <OrbitControls
+        enableZoom={false}
+      />
+      <Ball imgUrl={icon} />
+    </Suspense>
+    <Preload all />
+  </Canvas>
+)
+
+export default BallCanvas
